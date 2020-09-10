7ffd30211761632339b9ae40b9dce1ddfcc93232
stefan_adrian <39777353+games4life71@users.noreply.github.com>
2020-09-10 03:13:29 +0300

    Update Form1.cs
    -implemented FloofFill algo that uncovers all clear spaces 
    -fixed win/lose bug'
    -added some new features

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MineSweeper
@@ -17,84 +11,99 @@ namespace MineSweeper
             InitializeComponent();
         }
         private static List<object> mines = new List<object>();
-        private static int NumberOfMines; 
-
+        private static int NumberOfMines;
+        private static int FirstClick = 0;
         private void SetGameDifficulty()
         {
             NumberOfMines = meniu.Difficulty;
 
         }
 
-        private static void HideMap(TableLayoutPanel panel )
+        private static void HideMap(TableLayoutPanel panel)
         {
 
 
 
         }
-        
-        private  static int CountFreeSpaces(TableLayoutPanel panel , int col , int row)
-        {
-                //count  all the free spaces on every direction 
 
-            int[] y_pos = { -1, -1, -1, 0, 0, 1, 1, 1};
+        private static int CountFreeSpaces(TableLayoutPanel panel, int col, int row)
+        {
+            //count  all the free spaces on every direction 
+
+            int[] y_pos = { -1, -1, -1, 0, 0, 1, 1, 1 };
             int[] x_pos = { -1, 0, 1, -1, 1, -1, 0, 1 };
             int freeSpaces = 0;
-
             for (int i = 0; i < 8; i++)
             {
-                
+
                 //check all the neighbours 
 
-               if(x_pos[i]+col >= 0 && y_pos[i]+row>=0 && x_pos[i] + col <=19 && y_pos[i] + row<=19 )
+                if (x_pos[i] + col >= 0 && y_pos[i] + row >= 0 && x_pos[i] + col <= 19 && y_pos[i] + row <= 19)
                 {
-                    if (panel.GetControlFromPosition(x_pos[i] + col, y_pos[i] + row).Tag == "clear") freeSpaces++;
-                        //Console.WriteLine("u pressed on column {0} , and row {1} ", col+x_pos[i], row+y_pos[i]);
+                    if (panel.GetControlFromPosition(x_pos[i] + col, y_pos[i] + row).Tag != "mine"   ) freeSpaces++;
+                    //Console.WriteLine("u pressed on column {0} , and row {1} ", col+x_pos[i], row+y_pos[i]);
+
                 }
-
-
             }
+
+
             return freeSpaces;
+
+
         }
 
-        private static void ShowFreeSpaces(TableLayoutPanel panel, int col, int row)
+       
+        private static void UncoverCells(TableLayoutPanel panel, int col, int row)
         {
 
-             
-            Queue<Button> q = new Queue<Button>();
-            panel.GetControlFromPosition(col, row).Tag = "clearSHOW";
-            q.Enqueue((Button)panel.GetControlFromPosition(col, row));
-            while (q.Count != 0)
-            { 
-                //enqueue the first clear space and show it  
-                Button btn = q.Dequeue();
-                
-                //check all the adjiacent cells 
+            //////Button btn = (Button)panel.GetControlFromPosition(col, row);
+            //base cases
+            if (col > 19 || row > 19 || col < 0 || row < 0) return;
 
-                int[] y_pos = { -1, -1, -1, 0, 0, 1, 1, 1,};
-                int[] x_pos = { -1, 0, 1, -1, 1, -1, 0, 1 };
+           
+            if ( panel.GetControlFromPosition(col, row).Tag == "clearShow")
+            {
+                return;
+            }
+            if (panel.GetControlFromPosition(col, row).Tag == "mine")
 
-                for (int i = 0; i < 8; i++)
-                {
-                    if (x_pos[i] + col >= 0 && y_pos[i] + row >= 0 && x_pos[i] + col <= 19 && y_pos[i] + row <= 19)
-                    {
-                        if (panel.GetControlFromPosition(col + x_pos[i], row + y_pos[i]).Tag == "clear" && CountFreeSpaces(panel, col + x_pos[i], row + y_pos[i]) == 8)
-                        {
-                            Console.WriteLine(col + x_pos[i] + "pozitie");
-                            q.Enqueue((Button)panel.GetControlFromPosition(col + x_pos[i], row + y_pos[i]));
-                            panel.GetControlFromPosition(col + x_pos[i], row + y_pos[i]).Tag = "clearSHOW";
-
-                        }
-                    }
-                }
-               
-             
+            {
+                return;
             }
 
-        }           
-        
-      private void Form1_Load(object sender, EventArgs e)
+            if (CountFreeSpaces(panel, col, row) < 8)
+            {
+                panel.GetControlFromPosition(col, row).Text = (8 - CountFreeSpaces(panel, col, row)).ToString();
+                if (col == 19 || row == 19 || col == 0 || row == 0)
+                {
+                    panel.GetControlFromPosition(col, row).Text = CheckBombs(panel, col, row).ToString();
+                    if (CheckBombs(panel, col, row) == 0) { panel.GetControlFromPosition(col, row).Enabled = false; panel.GetControlFromPosition(col, row).Text = ""; }
+
+                }
+
+                return;
+            }
+            int noBombs = 8 - CountFreeSpaces(panel, col, row);
+            if (noBombs > 0) panel.GetControlFromPosition(col, row).Text = noBombs.ToString();
+
+            panel.GetControlFromPosition(col, row).Text = "..";
+            panel.GetControlFromPosition(col, row).Enabled = false;
+            panel.GetControlFromPosition(col, row).Tag = "clearShow";
+
+            UncoverCells(panel, col , row-1);
+            UncoverCells(panel, col-1, row );
+            UncoverCells(panel, col + 1, row);  
+            UncoverCells(panel, col , row+1);
+
+            UncoverCells(panel, col-1 , row-1);
+            UncoverCells(panel, col + 1 , row-1);
+            UncoverCells(panel, col -1, row+1);
+            UncoverCells(panel, col +1, row+1);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
         {
-            
+
 
             //dynamically adding buttons 
             for (int i = 0; i < panel.RowCount; i++)
@@ -105,15 +114,16 @@ namespace MineSweeper
                     button.Margin = new Padding(0);
                     button.Dock = DockStyle.Fill;
                     button.AutoSize = false;
-                    button.MouseUp+= mouse_click;
-                    panel.Controls.Add(button);
+                    button.MouseUp += mouse_click;
                     
+                    panel.Controls.Add(button);
+
                     // button.Click += EVENT    
 
                     //generating mines randomly  and setting button's tag to mine 
 
                 }
-           
+
             SetGameDifficulty();
             ClearMap(panel);
             GenerateMap(panel);
@@ -122,13 +132,15 @@ namespace MineSweeper
         private static void ClearMap(TableLayoutPanel panel)
         {
 
-            for(int i =  0; i<panel.RowCount;i++)
+            for (int i = 0; i < panel.RowCount; i++)
             {
 
-                for(int j = 0; j<panel.ColumnCount;j++)
+                for (int j = 0; j < panel.ColumnCount; j++)
                 {
                     Button btn = (Button)panel.GetControlFromPosition(j, i);
                     btn.BackgroundImage = null;
+                    btn.Enabled = true;
+                    btn.Text = "";
                     btn.Tag = "clear";
                 }
 
@@ -142,23 +154,23 @@ namespace MineSweeper
         /// A function that randomly generates a Minesweeper map 
         /// </summary>
         /// <param name="panel"></param>
-        private  void GenerateMap(TableLayoutPanel panel)
+        private void GenerateMap(TableLayoutPanel panel)
         {
             ClearMap(panel);
-            int numMines = NumberOfMines;
+            int numMines = 50;
             int i = 0;
-             while(numMines>0 )
+            while (numMines > 0)
             {
-            bool isPlaced = false; 
+                bool isPlaced = false;
                 Random rnd = new Random();
                 do
                 {
                     int rnd_x = rnd.Next(0, 20);
                     int rnd_y = rnd.Next(0, 20);
-                    Button btn =(Button)panel.GetControlFromPosition(rnd_y, rnd_x); 
+                    Button btn = (Button)panel.GetControlFromPosition(rnd_y, rnd_x);
                     if (btn.Tag == "clear")
-                    {                      
-                       btn.BackgroundImage = Properties.Resources.bomb;
+                    {
+                        // btn.BackgroundImage = Properties.Resources.bomb;
                         btn.BackgroundImageLayout = ImageLayout.Stretch;
                         btn.Tag = "mine";
                         isPlaced = true;
@@ -171,37 +183,51 @@ namespace MineSweeper
 
 
         }
-        private static void ShowMines(TableLayoutPanel panel )
+        private static void ShowMines(TableLayoutPanel panel)
         {
 
-            foreach (Button btn  in mines)
+            foreach (Button btn in mines)
             {
 
                 btn.BackgroundImage = Properties.Resources.bomb;
-                
+
             }
             mines.Clear();
         }
 
-        private void mouse_click (object sender  ,MouseEventArgs e )
+        private void mouse_click(object sender, MouseEventArgs e)
         {
 
-           if (sender is Button )
+            if (sender is Button)
             {
                 if (e.Button == MouseButtons.Left)
                 {
                     //LEFT CLICK 
                     // mine this spot 
-                  
-                    
+
+
                     Button btn = (Button)sender;
                     if (btn.Tag == "mine")
                     {
-                        //game is lost 
-                        //uncover all the mines 
-                        ShowMines(panel);
-                        MessageBox.Show("You have lost the game",  "Lost the game", MessageBoxButtons.OK, MessageBoxIcon.Information); 
-                       
+                        if (FirstClick == 0)
+                        {
+                            //move the mine to the left or right corner 
+                            btn.Tag = "clear";
+                            panel.GetControlFromPosition(19, 0).Tag = "mine";
+
+                            FirstClick++;
+                        }
+                        else
+                        {
+                            //game is lost 
+                            //uncover all the mines 
+                            ShowMines(panel);
+                            MessageBox.Show("You have lost the game", "Lost the game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            MessageBox.Show("Restart ? ", "Restart", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            ClearMap(panel);
+                            GenerateMap(panel);
+                        }
+
                     }
                     else
                     {
@@ -210,10 +236,12 @@ namespace MineSweeper
                         int col = panel.GetPositionFromControl(btn).Column;
                         int row = panel.GetPositionFromControl(btn).Row;
                         //Console.WriteLine("u pressed on column {0} , and row {1} " , col,row);
-                        ShowFreeSpaces(panel, col, row); 
-                        Console.WriteLine(CountFreeSpaces(panel ,col , row ));
-
+                        UncoverCells(panel, col, row);
                         
+                        int clearShow = 0;
+                        Console.WriteLine(CountFreeSpaces(panel, col, row));
+
+
                     }
 
 
@@ -221,11 +249,13 @@ namespace MineSweeper
 
                 else if (e.Button == MouseButtons.Right)
                 {     //RIGHT CLICK 
-                    //flag the spot 
-                   
+                      //flag the spot 
+                        
                     Button btn = (Button)sender;
+                    
                     btn.BackgroundImage = Properties.Resources.flag;
                     btn.BackgroundImageLayout = ImageLayout.Stretch;
+                   
                 }
 
             }
@@ -234,10 +264,35 @@ namespace MineSweeper
 
 
         }
+        private static int  CheckBombs(TableLayoutPanel panel , int col , int row )
+        {
 
+
+            int[] y_pos = { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] x_pos = { -1, 0, 1, -1, 1, -1, 0, 1 };
+            int bombs = 0; 
+            for (int i = 0; i < 8; i++)
+            {
+
+                //check all the neighbours 
+
+                if (x_pos[i] + col >= 0 && y_pos[i] + row >= 0 && x_pos[i] + col <= 19 && y_pos[i] + row <= 19)
+                {
+                    if (panel.GetControlFromPosition(x_pos[i] + col, y_pos[i] + row).Tag == "mine") bombs++;
+                    //Console.WriteLine("u pressed on column {0} , and row {1} ", col+x_pos[i], row+y_pos[i]);
+
+                }
+            }
+
+
+            return bombs;
+
+
+
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            GenerateMap(panel); 
+            GenerateMap(panel);
         }
 
         private void restartGameToolStripMenuItem_Click(object sender, EventArgs e)
@@ -252,7 +307,7 @@ namespace MineSweeper
 
         private void mainMenuToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if(MessageBox.Show("Are you sure you want to quit ?" , "Main menu" , MessageBoxButtons.YesNo , MessageBoxIcon.Question) == DialogResult.Yes)
+            if (MessageBox.Show("Are you sure you want to quit ?", "Main menu", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
 
                 var form = new meniu();
@@ -265,13 +320,14 @@ namespace MineSweeper
         private void restartGameToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             ShowMines(panel);
-             
-              if ( MessageBox.Show("Are you sure you want to restart ?", " Restart game ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes )
-                {
+
+
+            if (MessageBox.Show("Are you sure you want to restart ?", " Restart game ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
                 //restart the game 
                 ClearMap(panel);
                 GenerateMap(panel);
-               }
+            }
         }
-    } 
     }
+}
